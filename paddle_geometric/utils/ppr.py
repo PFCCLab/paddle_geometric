9f7c9911a42b4c9e3aef4e68cfe42c5a2b7bf240
zhiminzhang0830 <452516515@qq.com>
2025-11-04 09:55:20 +0000

    update ppr

--- paddle_geometric/utils/ppr.py
+++ paddle_geometric/utils/ppr.py
@@ -3,7 +3,9 @@ from typing import Callable, List, Optional, Tuple
 
 import numpy as np
 import paddle
-from paddle import Tensor
+
+from paddle_geometric import EdgeIndex
+from paddle_geometric.utils.num_nodes import maybe_num_nodes
 
 try:
     import numba
@@ -12,49 +14,49 @@ except Exception:  # pragma: no cover
     WITH_NUMBA = False
 
 
-def _get_ppr(  # pragma: no cover
+def _get_ppr(
     rowptr: np.ndarray,
     col: np.ndarray,
     alpha: float,
     eps: float,
     target: Optional[np.ndarray] = None,
 ) -> Tuple[List[List[int]], List[List[float]]]:
-
     num_nodes = len(rowptr) - 1 if target is None else len(target)
     alpha_eps = alpha * eps
     js = [[0]] * num_nodes
-    vals = [[0.]] * num_nodes
-
+    vals = [[0.0]] * num_nodes
     for inode_uint in numba.prange(num_nodes):
-        inode = inode_uint if target is None else target[inode_uint]
-
+        if target is None:
+            inode = numba.int64(inode_uint)
+        else:
+            inode = target[inode_uint]
         p = {inode: 0.0}
         r = {}
         r[inode] = alpha
         q = [inode]
-
         while len(q) > 0:
             unode = q.pop()
-
             res = r[unode] if unode in r else 0
-            p[unode] = p.get(unode, 0) + res
-
+            if unode in p:
+                p[unode] += res
+            else:
+                p[unode] = res
             r[unode] = 0
             start, end = rowptr[unode], rowptr[unode + 1]
             ucount = end - start
-
             for vnode in col[start:end]:
                 _val = (1 - alpha) * res / ucount
-                r[vnode] = r.get(vnode, 0) + _val
-
-                res_vnode = r[vnode]
+                if vnode in r:
+                    r[vnode] += _val
+                else:
+                    r[vnode] = _val
+                res_vnode = r[vnode] if vnode in r else 0
                 vcount = rowptr[vnode + 1] - rowptr[vnode]
-                if res_vnode >= alpha_eps * vcount and vnode not in q:
-                    q.append(vnode)
-
+                if res_vnode >= alpha_eps * vcount:
+                    if vnode not in q:
+                        q.append(vnode)
         js[inode_uint] = list(p.keys())
         vals[inode_uint] = list(p.values())
-
     return js, vals
 
 
@@ -62,13 +64,13 @@ _get_ppr_numba: Optional[Callable] = None
 
 
 def get_ppr(
-    edge_index: Tensor,
+    edge_index: paddle.Tensor,
     alpha: float = 0.2,
-    eps: float = 1e-5,
-    target: Optional[Tensor] = None,
+    eps: float = 1e-05,
+    target: Optional[paddle.Tensor] = None,
     num_nodes: Optional[int] = None,
-) -> Tuple[Tensor, Tensor]:
-    r"""Calculates the personalized PageRank (PPR) vector for all or a subset
+) -> Tuple[paddle.Tensor, paddle.Tensor]:
+    """Calculates the personalized PageRank (PPR) vector for all or a subset
     of nodes using a variant of the `Andersen algorithm
     <https://mathweb.ucsd.edu/~fan/wp/localpartition.pdf>`_.
 
@@ -85,33 +87,27 @@ def get_ppr(
 
     :rtype: (:class:`paddle.Tensor`, :class:`paddle.Tensor`)
     """
-    if not WITH_NUMBA:  # pragma: no cover
+    if not WITH_NUMBA:
         raise ImportError("'get_ppr' requires the 'numba' package")
-
     global _get_ppr_numba
     if _get_ppr_numba is None:
         _get_ppr_numba = numba.jit(nopython=True, parallel=True)(_get_ppr)
-
-    num_nodes = num_nodes or edge_index.shape[1] if num_nodes is None else num_nodes
-
-    rowptr, col = edge_index[0].numpy(), edge_index[1].numpy()
-
+    num_nodes = maybe_num_nodes(edge_index, num_nodes)
+    edge_index = EdgeIndex(edge_index, sparse_size=(num_nodes, num_nodes))
+    edge_index = edge_index.sort_by("row")[0]
+    (rowptr, col), _ = edge_index.get_csr()
     cols, weights = _get_ppr_numba(
-        rowptr,
-        col,
+        rowptr.cpu().numpy(),
+        col.cpu().numpy(),
         alpha,
         eps,
-        None if target is None else target.numpy(),
+        None if target is None else target.cpu().numpy(),
     )
-
     device = edge_index.place
-    col = paddle.to_tensor(list(chain.from_iterable(cols)), place=device)
-    weight = paddle.to_tensor(list(chain.from_iterable(weights)), place=device)
-    deg = paddle.to_tensor([len(value) for value in cols], place=device)
-
-    row = paddle.arange(num_nodes) if target is None else target
-    row = row.tile([deg.sum() // num_nodes])
-
-    edge_index = paddle.stack([row, col], axis=0)
-
+    col = paddle.tensor(list(chain.from_iterable(cols)), device=device)
+    weight = paddle.tensor(list(chain.from_iterable(weights)), device=device)
+    deg = paddle.tensor([len(value) for value in cols], device=device)
+    row = paddle.arange(end=num_nodes) if target is None else target
+    row = row.repeat_interleave(repeats=deg)
+    edge_index = paddle.stack(x=[row, col], axis=0)
     return edge_index, weight
