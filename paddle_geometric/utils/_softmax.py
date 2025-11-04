87d20c1fb0fd96c028d2706cfdde60367491dce8
zhiminzhang0830 <452516515@qq.com>
2025-11-04 06:25:39 +0000

    update softmax

--- paddle_geometric/utils/_softmax.py
+++ paddle_geometric/utils/_softmax.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import paddle
-from paddle import Tensor
 
 import paddle_geometric.typing
 from paddle_geometric import is_compiling
@@ -11,56 +10,70 @@ from paddle_geometric.utils.num_nodes import maybe_num_nodes
 
 
 def softmax(
-    src: Tensor,
-    index: Optional[Tensor] = None,
-    ptr: Optional[Tensor] = None,
+    src: paddle.Tensor,
+    index: Optional[paddle.Tensor] = None,
+    ptr: Optional[paddle.Tensor] = None,
     num_nodes: Optional[int] = None,
-    axis: int = 0,
-) -> Tensor:
-    r"""Computes a sparsely evaluated softmax.
+    dim: int = 0,
+) -> paddle.Tensor:
+    """Computes a sparsely evaluated softmax.
     Given a value tensor :attr:`src`, this function first groups the values
     along the first dimension based on the indices specified in :attr:`index`,
     and then proceeds to compute the softmax individually for each group.
 
     Args:
         src (Tensor): The source tensor.
-        index (Tensor, optional): The indices of elements for applying the
+        index (LongTensor, optional): The indices of elements for applying the
             softmax. (default: :obj:`None`)
-        ptr (Tensor, optional): If given, computes the softmax based on
+        ptr (LongTensor, optional): If given, computes the softmax based on
             sorted inputs in CSR representation. (default: :obj:`None`)
         num_nodes (int, optional): The number of nodes, *i.e.*
             :obj:`max_val + 1` of :attr:`index`. (default: :obj:`None`)
-        axis (int, optional): The dimension in which to normalize.
+        dim (int, optional): The dimension in which to normalize.
             (default: :obj:`0`)
 
     :rtype: :class:`Tensor`
+
+    Examples:
+        >>> src = torch.tensor([1., 1., 1., 1.])
+        >>> index = torch.tensor([0, 0, 1, 2])
+        >>> ptr = torch.tensor([0, 2, 3, 4])
+        >>> softmax(src, index)
+        tensor([0.5000, 0.5000, 1.0000, 1.0000])
+
+        >>> softmax(src, None, ptr)
+        tensor([0.5000, 0.5000, 1.0000, 1.0000])
+
+        >>> src = torch.randn(4, 4)
+        >>> ptr = torch.tensor([0, 4])
+        >>> softmax(src, index, dim=-1)
+        tensor([[0.7404, 0.2596, 1.0000, 1.0000],
+                [0.1702, 0.8298, 1.0000, 1.0000],
+                [0.7607, 0.2393, 1.0000, 1.0000],
+                [0.8062, 0.1938, 1.0000, 1.0000]])
     """
     if (ptr is not None and src.place.is_cpu_place()
-            and paddle_geometric.typing.WITH_SOFTMAX
-            and not is_compiling()):  # pragma: no cover
-        return pyg_lib.ops.softmax_csr(src, ptr, axis)
-
-    if (ptr is not None and
-        (ptr.dim() == 1 or (ptr.dim() > 1 and index is None) or
-         (paddle_geometric.typing.WITH_PADDLE_SCATTER and not is_compiling()))):
-
-        axis = axis + src.dim() if axis < 0 else axis
-        size = ([1] * axis) + [-1]
+            and paddle_geometric.typing.WITH_SOFTMAX and not is_compiling()):
+        return pyg_lib.ops.softmax_csr(src, ptr, dim)
+    if ptr is not None and (ptr.dim() == 1 or ptr.dim() > 1 and index is None
+                            or paddle_geometric.typing.WITH_PADDLE_SCATTER
+                            and not is_compiling()):
+        dim = dim + src.dim() if dim < 0 else dim
+        size = [1] * dim + [-1]
         count = ptr[1:] - ptr[:-1]
-        ptr = ptr.reshape(size)
-        src_max = segment(src.detach(), ptr, reduce='max')
-        src_max = paddle.repeat_interleave(src_max, repeats=count, axis=axis)
-        out = paddle.exp(src - src_max)
-        out_sum = segment(out, ptr, reduce='sum') + 1e-16
-        out_sum = paddle.repeat_interleave(out_sum, repeats=count, axis=axis)
+        ptr = ptr.view(size)
+        src_max = segment(src.detach(), ptr, reduce="max")
+        src_max = src_max.repeat_interleave(repeats=count, axis=dim)
+        out = (src - src_max).exp()
+        out_sum = segment(out, ptr, reduce="sum") + 1e-16
+        out_sum = out_sum.repeat_interleave(repeats=count, axis=dim)
     elif index is not None:
         N = maybe_num_nodes(index, num_nodes)
-        src_max = scatter(src.detach(), index, axis, dim_size=N, reduce='max')
-        out = src - paddle.index_select(src_max, index=index, axis=axis)
-        out = paddle.exp(out)
-        out_sum = scatter(out, index, axis, dim_size=N, reduce='sum') + 1e-16
-        out_sum = paddle.index_select(out_sum, index=index, axis=axis)
+        src_max = scatter(src.detach(), index, dim, dim_size=N, reduce="max")
+        out = src - src_max.index_select(axis=dim, index=index)
+        out = out.exp()
+        out_sum = scatter(out, index, dim, dim_size=N, reduce="sum") + 1e-16
+        out_sum = out_sum.index_select(axis=dim, index=index)
     else:
         raise NotImplementedError("'softmax' requires 'index' to be specified")
-
     return out / out_sum
