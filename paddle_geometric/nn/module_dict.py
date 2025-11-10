7cbf68f966b6dfd02a416648b91bc7e0cfe79d5d
zhiminzhang0830 <452516515@qq.com>
2025-11-10 03:59:20 +0000

    update module_dict

--- paddle_geometric/nn/module_dict.py
+++ paddle_geometric/nn/module_dict.py
@@ -1,60 +1,49 @@
 from typing import Final, Iterable, Mapping, Optional, Tuple, Union
 
 import paddle
-from paddle.nn import Layer
 
 Key = Union[str, Tuple[str, ...]]
 
 
-# `paddle.nn.LayerDict` doesn't allow `.` to be used in key names.
-# This `LayerDict` will support it by converting the `.` to `#` in the
-# internal representation and converts it back to `.` in the external
-# representation. It also allows passing tuples as keys.
 class ModuleDict(paddle.nn.LayerDict):
     CLASS_ATTRS: Final[Tuple[str, ...]] = tuple(dir(paddle.nn.LayerDict))
 
     def __init__(
         self,
-        layers: Optional[Mapping[Union[str, Tuple[str, ...]], Layer]] = None,
+        modules: Optional[Mapping[Union[str, Tuple[str, ...]],
+                                  paddle.nn.Layer]] = None,
     ):
-        if layers is not None:  # Replace the keys in layers:
-            layers = {
-                self.to_internal_key(key): layer
-                for key, layer in layers.items()
+        if modules is not None:
+            modules = {
+                self.to_internal_key(key): module
+                for key, module in modules.items()
             }
-        super().__init__(layers)
+        super().__init__(modules)
 
     @classmethod
     def to_internal_key(cls, key: Key) -> str:
-        if isinstance(key, tuple):  # LayerDict can't handle tuples as keys
+        if isinstance(key, tuple):
             assert len(key) > 1
             key = f"<{'___'.join(key)}>"
         assert isinstance(key, str)
-
-        # LayerDict cannot handle keys that exist as class attributes:
         if key in cls.CLASS_ATTRS:
-            key = f'<{key}>'
-
-        # LayerDict cannot handle dots in keys:
-        return key.replace('.', '#')
+            key = f"<{key}>"
+        return key.replace(".", "#")
 
     @classmethod
     def to_external_key(cls, key: str) -> Key:
-        key = key.replace('#', '.')
-
-        if key[0] == '<' and key[-1] == '>' and key[1:-1] in cls.CLASS_ATTRS:
+        key = key.replace("#", ".")
+        if key[0] == "<" and key[-1] == ">" and key[1:-1] in cls.CLASS_ATTRS:
             key = key[1:-1]
-
-        if key[0] == '<' and key[-1] == '>' and '___' in key:
-            key = tuple(key[1:-1].split('___'))
-
+        if key[0] == "<" and key[-1] == ">" and "___" in key:
+            key = tuple(key[1:-1].split("___"))
         return key
 
-    def __getitem__(self, key: Key) -> Layer:
+    def __getitem__(self, key: Key) -> paddle.nn.Layer:
         return super().__getitem__(self.to_internal_key(key))
 
-    def __setitem__(self, key: Key, layer: Layer):
-        return super().__setitem__(self.to_internal_key(key), layer)
+    def __setitem__(self, key: Key, module: paddle.nn.Layer):
+        return super().__setitem__(self.to_internal_key(key), module)
 
     def __delitem__(self, key: Key):
         return super().__delitem__(self.to_internal_key(key))
@@ -65,5 +54,5 @@ class ModuleDict(paddle.nn.LayerDict):
     def keys(self) -> Iterable[Key]:
         return [self.to_external_key(key) for key in super().keys()]
 
-    def items(self) -> Iterable[Tuple[Key, Layer]]:
+    def items(self) -> Iterable[Tuple[Key, paddle.nn.Layer]]:
         return [(self.to_external_key(k), v) for k, v in super().items()]
